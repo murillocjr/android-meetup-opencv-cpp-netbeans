93d16cef1a7b85db706a6241b25eaad959658311
Jorge Murillo <jorgermurilloc@gmail.com>
2018-04-28 09:10:48 -0500

    red_blue

--- AreaCalculator.cpp
+++ AreaCalculator.cpp
@@ -4,20 +4,79 @@
  * and open the template in the editor.
  */
 
-/* 
+/*
  * File:   AreaCalculator.cpp
  * Author: netmaster
- * 
+ *
  * Created on April 22, 2018, 5:18 PM
  */
 
 #include "AreaCalculator.h"
 
+
+int redCount(Mat hsv)
+{
+    Mat1b mask1, mask2;
+    inRange(hsv, Scalar(0, 70, 50), Scalar(10, 255, 255), mask1);
+    inRange(hsv, Scalar(170, 70, 50), Scalar(180, 255, 255), mask2);
+
+    Mat1b mask = mask1 | mask2;
+
+    imshow("Mask", mask);
+    waitKey();
+
+    int s = cv::sum( mask )[0];
+    s=s/255;
+
+    //cout<<hsv.cols<<","<<hsv.rows<<","<<s<<"...."<<endl;
+
+    return s;
+}
+
+int blueCount(Mat hsv)
+{
+    Mat1b mask;
+    inRange(hsv, Scalar(110, 50, 50), Scalar(130, 255, 255), mask);
+
+
+    imshow("Mask", mask);
+    waitKey();
+
+    int s = cv::sum( mask )[0];
+    s=s/255;
+
+    //cout<<hsv.cols<<","<<hsv.rows<<","<<s<<"...."<<endl;
+
+    return s;
+}
+
 String AreaCalculator::processMat(Mat frame)
 {
     
-    ostringstream os;
-    os << frame.cols;
+     
+    int factor = 10; 
+    resize(frame,frame,cvSize((frame.cols)/factor,(frame.rows)/factor)); 
+
+    Mat3b hsv;
+    cvtColor(frame, hsv, COLOR_BGR2HSV);
+
+    Vec3b color = hsv.at<Vec3b>(Point(200,300));
     
+    int red = redCount(hsv.clone());
+    int blue = blueCount(hsv.clone());
+
+
+    ostringstream os;
+    os<<color;
+    os<<",";
+    os<<red;
+    os<<",";
+    os<<blue;
+    os<<",";
+    os<<frame.cols;
+    os<<",";
+    os<<frame.rows;
+
     return os.str();;
 }
+
--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char** argv) {
     Mat colorFrame;
     AreaCalculator areaCalculator;
 
-    colorFrame = imread("/home/oamakas/projects/sampleCode/android-ndk/Camera2Basic/IMG_20180422_181646.jpg", CV_LOAD_IMAGE_ANYCOLOR);
+    colorFrame = imread("/home/oamakas/projects/sampleCode/android-ndk/Camera2Basic/1b3r.jpg", CV_LOAD_IMAGE_ANYCOLOR);
    
     cout << areaCalculator.processMat(colorFrame);
 
